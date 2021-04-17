602bd7d94265faac90a9a6b77e040ed7ad1071d9
Magno Belloni <bellonidesouza6@gmail.com>
2021-04-17 19:26:47 -0300

    Redirecionamento de / para /swagger

---
+++ TarefasBackEnd/Controllers/IndexController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TarefasBackEnd.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
